db859d373c4ba7fc66de15823644e65fde20a6d7
Alexis Huerta <71994091+alexis-huerta@users.noreply.github.com>
2020-12-21 18:04:38 +0000

    worked in login validations

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { SignInModalComponent } from './components/sign-in-modal/sign-in-modal.component';
 
@@ -8,8 +9,31 @@ import { SignInModalComponent } from './components/sign-in-modal/sign-in-modal.c
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  errorMessage = {
+    'email': [
+      { type: 'required', message: 'Ingrese el correo' },
+      { type: 'maxlength', message: 'El correo no debe ser mayor a 30 caracteres' },
+      { type: 'pattern', message: 'Porfavor ingrese un correo valido' }
+    ],
+    'password': [
+      { type: 'required', message: 'Ingrese la contraseña' },
+      { type: 'minlength', message: 'La contraseña debe ser mayor a 5 caracteres' },
+    ]
+  }
+  loginForm: FormGroup;
   
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog,  public formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.maxLength(30),
+        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.minLength(5)
+      ]]
+    });
   }
 
 
@@ -22,6 +46,17 @@ export class LoginPageComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('dialog result', result);
     });
+  }
+
+  getErrorMessage(fild: string, error: string): boolean {
+    return this.loginForm.get(fild).hasError(error);
+ 
+     
+  }
+
+  isValidFild(fild: string): boolean {
+    return (this.loginForm.get(fild).touched || this.loginForm.get(fild).dirty)
+    && !this.loginForm.get(fild).valid
   }
 
 
