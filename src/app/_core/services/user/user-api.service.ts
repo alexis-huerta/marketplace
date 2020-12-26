3f15639161782faafeefd30303a835161453d051
Alexis Huerta <71994091+alexis-huerta@users.noreply.github.com>
2020-12-26 16:05:02 +0000

    url

--- src/app/_core/services/user/user-api.service.ts
+++ src/app/_core/services/user/user-api.service.ts
@@ -8,8 +8,8 @@ import { BehaviorSubject, Subject } from 'rxjs';
   providedIn: 'root'
 })
 export class UserApiService {
-  //url = "http://localhost:3000/";
-  url = " https://themarketplace2020.herokuapp.com/";
+  url = "http://localhost:3000/";
+  //url = " https://themarketplace2020.herokuapp.com/";
   currentUser;
   isLogged$ = false;
 
